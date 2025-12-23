4f6b4661212b574a0fd1c4a1c36e47923bf24878
Dari <156703870+GirlWithLimon@users.noreply.github.com>
2025-12-23 09:37:36 +0300

    .

--- test9/src/First.java
+++ test9/src/First.java
@@ -1,2 +1,47 @@
 public class First {
+     public static void main(String[] args) throws InterruptedException {
+            Object lock = new Object();
+            Object waitObj = new Object();
+
+            Thread thread = new Thread(() -> {
+                try {
+
+                        synchronized (lock) {
+                        Thread.sleep(1000);
+                    }
+
+                    synchronized (waitObj) {
+                        waitObj.wait();
+                    }
+                } catch (InterruptedException e) {
+                    e.printStackTrace();
+                }
+            });
+
+            System.out.println(thread.getState());
+
+            synchronized (lock) {
+                thread.start();
+                System.out.println(thread.getState());
+                Thread.sleep(100);
+                System.out.println(thread.getState());
+
+            }
+
+            Thread.sleep(150);
+
+            System.out.println(thread.getState());
+
+            Thread.sleep(1200);
+
+            System.out.println(thread.getState());
+
+            synchronized (waitObj) {
+                waitObj.notify();
+            }
+
+            thread.join();
+
+            System.out.println(thread.getState());
+     }
 }
---
+++ test9/src/Second.java
@@ -0,0 +1,47 @@
+public class Second{
+    private static final Object lock = new Object();
+    private static boolean firstThreadTurn = true;
+
+    public static void main(String[] args) {
+        Thread thread1 = new Thread(() -> {
+            for (int i = 0; i < 5; i++) {
+                synchronized (lock) {
+                    while (!firstThreadTurn) {
+                        try {
+                            lock.wait();
+                        } catch (InterruptedException e) {
+                            Thread.currentThread().interrupt();
+                            return;
+                        }
+                    }
+
+                    System.out.println("Поток-1");
+                    firstThreadTurn = false;
+                    lock.notifyAll();
+                }
+            }
+        });
+
+        Thread thread2 = new Thread(() -> {
+            for (int i = 0; i < 5; i++) {
+                synchronized (lock) {
+                    while (firstThreadTurn) {
+                        try {
+                            lock.wait();
+                        } catch (InterruptedException e) {
+                            Thread.currentThread().interrupt();
+                            return;
+                        }
+                    }
+
+                    System.out.println("Поток-2");
+                    firstThreadTurn = true;
+                    lock.notifyAll();
+                }
+            }
+        } );
+
+        thread1.start();
+        thread2.start();
+    }
+}
