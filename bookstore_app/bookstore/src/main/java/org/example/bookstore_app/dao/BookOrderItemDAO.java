18ec4e007ff247b5dcf579f211c8a6404da08e37
Dari <156703870+GirlWithLimon@users.noreply.github.com>
2026-08-27 10:28:19 +0300

    /

--- bookstore_app/bookstore/src/main/java/org/example/bookstore_app/dao/BookOrderItemDAO.java
+++ bookstore_app/bookstore/src/main/java/org/example/bookstore_app/dao/BookOrderItemDAO.java
@@ -1,13 +1,13 @@
 package org.example.bookstore_app.dao;
 
+import java.util.List;
+
 import org.example.bookstore_app.model.BookOrderItem;
 import org.hibernate.Session;
 import org.hibernate.query.Query;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Repository;
-import org.springframework.transaction.annotation.Transactional;
-import java.util.List;
 
 @Repository
 public class BookOrderItemDAO extends HibernateAbstractDao<BookOrderItem, Integer> {
--- bookstore_app/bookstore/src/main/java/org/example/bookstore_app/model/User.java
+++ bookstore_app/bookstore/src/main/java/org/example/bookstore_app/model/User.java
@@ -32,7 +32,7 @@ public class User implements Serializable {
         this.enabled = true;
     }
 
-     public Integer getId() { return id; }
+    public Integer getId() { return id; }
     public void setId(Integer id) { this.id = id; }
 
     public String getUsername() { return username; }
